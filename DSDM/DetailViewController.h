d57c1a794318d9455d5a1a90419008ebcb86b1f4
Molina <gaudy41@gmail.com>
2013-04-26 09:08:47 +0200

    Commit inicial. Creado el proyecto de Xcode para comenzar a trabajar.

---
+++ DSDM/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  DSDM
+//
+//  Created by Molina on 26/04/13.
+//  Copyright (c) 2013 Ladis-Molina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ DSDM/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  DSDM
+//
+//  Created by Molina on 26/04/13.
+//  Copyright (c) 2013 Ladis-Molina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
